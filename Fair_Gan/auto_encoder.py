26bed3ebe62ba7bb6b51a57c984321ecf9db3bd6
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-12-19 12:55:40 +0300

    Slopy ae added

--- Fair_Gan/auto_encoder.py
+++ Fair_Gan/auto_encoder.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn as nn
 import torch.nn.functional as F
-import types, typing
 from typing import Callable
 
+## The first dude uses weight tying therefore a bit cheaper. 
 class auto_encoder_bb(nn.Module):
     def __init__(self, 
                  dims:list[int], 
@@ -50,4 +50,35 @@ class auto_encoder_bb(nn.Module):
             return enc_output
         return self.__decoder__(enc_output)
 
-            
+## This dude is a bit slopy...
+class auto_encoder(nn.Module):
+    def __init__(self, 
+                 dims:list[int], 
+                 activation:Callable = nn.GELU("tanh"),
+                 **kwargs)->None:
+        super().__init__(**kwargs)
+        
+        self.dims = dims
+        self.depth = len(dims)
+        self.activation = activation
+        self.encoder = nn.ModuleList([
+            nn.Linear(dims[i], dims[i+1]) for i in range(self.depth-1)
+        ])
+        self.decoder = nn.ModuleList([
+            nn.Linear(dims[i+1], dims[i]) for i in reversed(range(self.depth-1))
+        ])
+
+    def forward(self, x:torch.Tensor, encoder_output = False):
+        for i, layer in enumerate(self.encoder):
+            x = layer(x)
+            if i < self.depth -1:
+                x = self.activation(x)
+        
+        if encoder_output:
+            return x
+
+        for i, layer in enumerate(self.decoder):
+            if i < self.depth -1:
+                x = self.activation(x)
+            x = layer(x)
+        return x
