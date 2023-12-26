1adbbfcadb3ea1791e9f58c9980ed9171b7e8cf5
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-12-26 14:09:04 +0300

    Almost done!

--- Fair_Gan/simple_tabular_gan.py
+++ Fair_Gan/simple_tabular_gan.py
@@ -1,14 +1,14 @@
 ## Below we implement a simple GAN for tabular data generation for experimental purposes!!!
 import pandas as pd
+import numpy as np
+import tqdm as tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision 
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
-
 @dataclass
 class training_setup:
     z_dim: str
@@ -20,6 +20,7 @@ class training_setup:
     batch_size:int = 32 
     num_epochs:int = 50
     seed: int = 0
+    save_dir: str = "generator_model"
 
 class Discriminator(nn.Module):
     def __init__(self, 
@@ -52,77 +53,94 @@ class Generator(nn.Module):
 
 
 def return_disc_gen(setup:dataclass = training_setup(10, 10, 10, "w")):
-    ## Grab the attributes of the
-    num_features, z_dim, device, batch_size = setup.num_features, setup.z_dim, setup.device, setup.batch_size
-    ## Disc and gen
+    ## Grab the attributes
+    num_features, z_dim, device = setup.num_features, setup.z_dim, setup.device
+    ## Disc and gen, 
     disc = Discriminator(num_features).to(device)
     gen = Generator(z_dim, num_features).to(device)
+
+    return disc, gen
+
+def prepare_data(setup: dataclass, 
+                numerical_features:list[int],
+                categorical_features:list[int],
+                split = True):
+    data = pd.read_csv(setup.data_dir)
+    data_x = data.iloc[:, numerical_features].to_numpy()  #RxF -- Rows x Features -> numpy_array
+    data_y = data.iloc[:, categorical_features].to_numpy()
+    normalized_data_x = (data_x - data_x.min(axis = 1))/(data_x.max(axis = 1) - data_x.min(axis = 1))
+    if split:
+        return train_test_split((normalized_data_x, data_y)) ## train_x, train_y, test_x, test_y splitted
+    return normalized_data_x, data_y
+
+def data_pipe_line(setup, data:tuple(np.ndarray, np.ndarray)):
+    return DataLoader(data, batch_size=setup.batch_size, shuffle=True)
+
+def train(setup: dataclass, 
+          training_data: torch.Tensor,
+          discriminator: nn.Module, 
+          generator: nn.Module):
+    ## 
+    device = setup.device
+    lr = setup.lr
+    num_epochs = setup.num_epochs
+    z_dim = setup.z_dim
+    ##
+    
+    opt_disc = optim.Adam(discriminator.parameters(), lr = lr)
+    opt_gen = optim.Adam(generator.parameters(), lr = lr)
+    loss_fn = nn.BCELoss()
     ## Fixing the seed is needed for reproducibility...
     torch.manual_seed(setup.seed)
     fixed_noise = torch.randn((batch_size, z_dim)).to(device)
-    return disc, gen, fixed_noise
 
-def prepare_data(setup: dataclass, 
-                 numerical_features:list[int],
-                categorical_features:list[int]):
-    pd_data = pd.read_csv(setup.data_dir)
-    ## Do some preprocessing here!!!
-    ## Normalize the data --- convert to tensor---
-    ## May wish to use the collating function 
-    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-def train():
-    ##Here training will happen here, and we will save the model at the end
-    return None
-
-
-loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-opt_disc = optim.Adam(disc.parameters(), lr=lr)
-opt_gen = optim.Adam(gen.parameters(), lr=lr)
-criterion = nn.BCELoss()
-
-for epoch in range(num_epochs):
-    for batch_idx, (real, _) in enumerate(loader):
-        real = real.view(-1, 784).to(device)
-        batch_size = real.shape[0]
+    for epoch in range(setup.num_epochs):
+        for batch_idx, (real, _) in enumerate(training_data):
+            real = real.to(setup.device)
+            batch_size = real.shape[0]
 
         ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
-        noise = torch.randn(batch_size, z_dim).to(device)
-        fake = gen(noise)
-        disc_real = disc(real).view(-1)
-        lossD_real = criterion(disc_real, torch.ones_like(disc_real))
-        disc_fake = disc(fake).view(-1)
-        lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
-        lossD = (lossD_real + lossD_fake) / 2
-        disc.zero_grad()
-        lossD.backward(retain_graph=True)
-        opt_disc.step()
+            noise = torch.randn(batch_size, z_dim).to(device)
+            fake = generator(noise)
+            disc_real = discriminator(real).view(-1)
+            lossD_real = loss_fn(disc_real, torch.ones_like(disc_real))
+            disc_fake = discriminator(fake).view(-1)
+            lossD_fake = loss_fn(disc_fake, torch.zeros_like(disc_fake))
+            lossD = (lossD_real + lossD_fake) / 2
+            discriminator.zero_grad()
+            lossD.backward(retain_graph=True)
+            opt_disc.step()
 
-        ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
-        # where the second option of maximizing doesn't suffer from
-        # saturating gradients
-        output = disc(fake).view(-1)
-        lossG = criterion(output, torch.ones_like(output))
-        gen.zero_grad()
-        lossG.backward()
-        opt_gen.step()
+            ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
+            # where the second option of maximizing doesn't suffer from
+            # saturating gradients
+            output = discriminator(fake).view(-1)
+            lossG = loss_fn(output, torch.ones_like(output))
+            generator.zero_grad()
+            lossG.backward()
+            opt_gen.step()
 
-        if batch_idx == 0:
-            print(
-                f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
-                      Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
-            )
+            if batch_idx == 0:
+                 print(
+                     f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
+                     Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
+                     )
 
             with torch.no_grad():
-                fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
-                data = real.reshape(-1, 1, 28, 28)
-                img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
-                img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+                fake = generator(fixed_noise)
+                ### do your magic here!!!!
+                ### logging some loss or something maybe some comparison would be good!!! 
+    try:
+        torch.save(generator.cpu(), "generator_weights.w")
+        print("Generator saved!!!!")
 
-                writer_fake.add_image(
-                    "Mnist Fake Images", img_grid_fake, global_step=step
-                )
-                writer_real.add_image(
-                    "Mnist Real Images", img_grid_real, global_step=step
-                )
-                step += 1
+    except Exception as e:
+        print(f"Some thing went wrong with {e}")
+    return None
+
+def main():
+    ### This dude is the main function we shall be using!!!
+    return None
+
+if __name__ == "__main__":
+    pass
